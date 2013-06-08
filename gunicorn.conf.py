c1f0810d8c37c10c5c6b91a738f960dbfbda8dcc
dnuske <dnuske@gmail.com>
2013-06-08 10:22:41 -0300

    deleted garbage files

--- gunicorn.conf.py
+++
@@ -1,5 +0,0 @@
-bind = "127.0.0.1:8888"
-workers = 3
-secure_scheme_headers={'X-FORWARDED-PROTOCOL': 'ssl', 'X-FORWARDED-SSL': 'on'}
-accesslog = "/var/www/ibiddjango/gunicorn.access.log"
-errorlog = "/var/www/ibiddjango/gunicorn.access.log"
