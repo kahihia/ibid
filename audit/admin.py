c4c0db57b0dc50dc6746c68193a791ad2b056407
Gabriel Patiño <gepatino+github@gmail.com>
2013-07-04 22:52:11 -0300

    Added an admin action to bulk delete items using audit

--- audit/admin.py
+++ audit/admin.py
@@ -3,3 +3,15 @@ from django.contrib import admin
 from models import Log
 
 admin.site.register(Log)
+
+
+# Change the default 'Delete selected items' behaviour so the delete() method
+# is executed on each object instead of the queryset, and the action is audited
+
+def audited_delete_selected(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.delete()
+audited_delete_selected.short_description = 'Delete selected items (audited)'
+
+admin.site.disable_action('delete_selected')
+admin.site.add_action(audited_delete_selected)
