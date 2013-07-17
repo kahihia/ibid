908a29c9713461c9a956359eec56038b8bd7edd2
dnuske <dnuske@gmail.com>
2013-07-17 18:20:20 -0300

    removed doble auction creation on auction fixtures

--- bidding/views/bidapi.py
+++ bidding/views/bidapi.py
@@ -41,8 +41,6 @@ def finish(request):
             if len(aifx):
                 rt = aifx[0].make_auctions()
                 print "rt = aifx[0].make_auctions()", rt
-                for auct in rt:
-                    client.auction_created(auct)
 
         if len(Auction.objects.filter(is_active=True).filter(status='precap').filter(bid_type='bid')) == 0:
             #run first bids fixture
@@ -50,8 +48,6 @@ def finish(request):
             if len(aifx):
                 rt = aifx[0].make_auctions()
                 print "rt = aifx[0].make_auctions()", rt
-                for auct in rt:
-                    client.auction_created(auct)
 
     return HttpResponse(json.dumps({'everyThingIsFine':True}))
 
