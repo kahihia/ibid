719091972765e1610424ea1a86045b356fbfc09d
dnuske <dnuske@gmail.com>
2013-08-12 10:49:18 -0300

    added message litener and sender to server

--- message/models.py
+++ message/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class EventLog(models.Model):
+    event = models.TextField()
+    data = models.TextField()
+    sender = models.TextField()
+    receiver = models.TextField()
+    transport = models.TextField()
+    timestamp = models.DateTimeField(null=True, blank=True)
+    event_id = models.IntegerField
+
+    def saveEvent(self, event):
+        self.event = event["event"]
+        self.data = event["data"]
+        self.sender = event["sender"]
+        self.receiver = event["receiver"]
+        self.transport = event["transport"]
+        self.timestamp = event["timestamp"]
+        self.event_id = event["id"]
+        self.save()
