c173f48aeab0a5c0e7090aa96e6e02c2cad9f5dc
Gabriel Patiño <gepatino@gmail.com>
2013-08-14 21:42:30 +0000

    Fixed typo in settings

--- settings_default.py
+++ settings_default.py
@@ -255,7 +255,7 @@ FACEBOOK_FORCE_PROFILE_UPDATE_ON_LOGIN = True
 FACEBOOK_REGISTRATION_BACKEND = 'ibiddjango.authbackends.YambidRegistration'
 FACEBOOK_AUTH_URL = 'https://www.facebook.com/dialog/oauth?client_id={app}&redirect_uri={url}&scope=email,publish_stream'
 FACEBOOK_APP_URL           = 'https://apps.facebook.com/{appname}/'
-FACEBOOK_AUTH_REDIRECT_URL = 'https://apps.facebook.com/{appname}/fb/login'
+FACEBOOK_AUTH_REDIRECT_URL = 'https://apps.facebook.com/{appname}/fb/login/'
 FACEBOOK_CANVAS_HOME_URL   = 'https://apps.facebook.com/{appname}/canvashome/' 
 
 
