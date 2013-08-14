a1c95bb3d632db54401bd935841b2ffabbdd5cb5
Alejandro García Iglesias <ale.garciaiglesias@gmail.com>
2013-08-14 19:22:54 -0300

    Partial implementation of reusable dialogs using directives. Should make a service to activate them.

--- static/js/directives.js
+++ static/js/directives.js
@@ -1,15 +1,31 @@
-angular
-  .module('app.directives', [])
-  .directive('ibAutoscroll', function () {
-    return {
-      require: 'ngModel',
-      link: function (scope, element, attributes, ngModel) {
-        scope.$watch(function () {
-          return ngModel.$modelValue;
-        }, function () {
-          element.scrollTop(element.get(0).scrollHeight);
-        },
-        true);
-      }
-    };
-  });
+(function () {
+    'use strict';
+
+    angular
+        .module('app.directives', [])
+        .directive('ibAutoscroll', function () {
+            return {
+                require: 'ngModel',
+                link: function postLink (scope, element, attributes, ngModel) {
+                    scope.$watch(function () {
+                        return ngModel.$modelValue;
+                    }, function () {
+                        element.scrollTop(element.get(0).scrollHeight);
+                    },
+                    true);
+                }
+            };
+        })
+        .directive('dialog', function () {
+            return {
+                replace: true,
+                restrict: 'E',
+                scope: {
+                    title: '@dialogTitle'
+                },
+                templateUrl: '/static/templates/dialog.html',
+                transclude: true,
+                link: function postLink (scope, element, attributes) {}
+            };
+        });
+}());
