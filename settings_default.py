d40b93aefff2ab32d5958852d853d85953fce8b8
Gabriel Patiño <gepatino@gmail.com>
2013-08-22 20:27:24 +0000

    removed authbackends

--- settings_default.py
+++ settings_default.py
@@ -254,7 +254,6 @@ FACEBOOK_APP_SECRET = ''
 FACEBOOK_APP_NAME = 'ibidgames'
 FACEBOOK_FORCE_PROFILE_UPDATE_ON_LOGIN = True
 FACEBOOK_REGISTRATION_BACKEND = 'registration.backends.default.DefaultBackend'
-#FACEBOOK_REGISTRATION_BACKEND = 'ibiddjango.authbackends.YambidRegistration'
 FACEBOOK_AUTH_URL = 'https://www.facebook.com/dialog/oauth?client_id={app}&redirect_uri={url}&scope=email,publish_stream'
 FACEBOOK_APP_URL           = 'https://apps.facebook.com/{appname}/'
 FACEBOOK_AUTH_REDIRECT_URL = 'https://apps.facebook.com/{appname}/fb/login/'
