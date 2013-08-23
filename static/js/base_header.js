c1abf97e58e06c2bd4af5316902e00f1672b2426
Alejandro García Iglesias <ale.garciaiglesias@gmail.com>
2013-08-23 16:22:50 -0300

    Re-add call to $scope.$apply() on "auction:finished" event handler (UserDetailsCtrl).

--- static/js/base_header.js
+++ static/js/base_header.js
@@ -57,18 +57,20 @@ function userDetailsCtrl($scope, $rootScope, $http) {
         }})
     });
     $rootScope.$on('auction:finished', function (event, auction) {
-        // If current user won, show win modal.
-        if (auction.winner.facebookId !== $scope.user.facebookId) {
-            return;
-        }
-        $scope.wonAuction = auction;
-        $scope.showWonTokensDialog = (auction.bidType === $scope.AUCTION_TYPE_TOKENS);
-        $scope.showWonItemDialog = (auction.bidType === $scope.AUCTION_TYPE_CREDITS);
-        // If playing for tokens, update user tokens.
-        if (auction.bidType !== $scope.AUCTION_TYPE_TOKENS) {
-            return;
-        }
-        $rootScope.user.tokens += Number(auction.retailPrice);
+        $scope.$apply(function () {
+            // If current user won, show win modal.
+            if (auction.winner.facebookId !== $scope.user.facebookId) {
+                return;
+            }
+            $scope.wonAuction = auction;
+            $scope.showWonTokensDialog = (auction.bidType === $scope.AUCTION_TYPE_TOKENS);
+            $scope.showWonItemDialog = (auction.bidType === $scope.AUCTION_TYPE_CREDITS);
+            // If playing for tokens, update user tokens.
+            if (auction.bidType !== $scope.AUCTION_TYPE_TOKENS) {
+                return;
+            }
+            $rootScope.user.tokens += Number(auction.retailPrice);
+        });
     });
     $rootScope.$on('user:friendJoined', $scope.showJoinedFriendsDialog);
 
