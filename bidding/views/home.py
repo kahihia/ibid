9ce2b7a6a5ff7fee6019907610a22a350d11abb2
dnuske <dnuske@gmail.com>
2013-08-29 20:29:27 -0300

    added a special key in settings for channel file domain to avoid doble backslash

--- bidding/views/home.py
+++ bidding/views/home.py
@@ -66,7 +66,7 @@ def canvashome(request):
                                {'fb_app_id': settings.FACEBOOK_APP_ID,
                                 'PUBNUB_PUB': settings.PUBNUB_PUB,
                                 'PUBNUB_SUB': settings.PUBNUB_SUB,
-                                'SITE_NAME': settings.SITE_NAME,
+                                'SITE_NAME_WOUT_BACKSLASH': settings.SITE_NAME_WOUT_BACKSLASH,
                                 'display_popup': display_popup,
                                 'facebook_user_id': member.facebook_id,
                                 'tosintro': FlatPage.objects.filter(title="tacintro")[0].content,
