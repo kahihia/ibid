60c5a6808839a9d787cb074359fc421106393f27
archnoth <archnoth@gmail.com>
2013-09-24 11:59:35 -0300

    model notification in apps/main

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import datetime
+from bidding.models import Member
 
-# Create your models here.
+NOTIFICATION_TYPES = (('FinishedAuction' , 'FinishedAuction'),
+                    ('',''))
+
+class Notification(models.Model):
+    To = models.ForeignKey(Member)
+    From = models.ForeignKey(Member,related_name='notifications', null=True)
+    Type = models.CharField(choices=NOTIFICATION_TYPES, null=False, blank=False, max_length=10, default='text')
+    Message = models.TextField(null=True, blank=True)
+    Created = models.DateTimeField(default=datetime.now)
+    Read = models.DateTimeField(default=None, null=True)
+    
+    def __unicode__(self):
+        return "To: %s, From: $s, Message: %s" % (self.To, self.From, self.Message)
