174ee889f1c66005be110b0b81e361c1b76dbe73
Alejandro García Iglesias <ale.garciaiglesias@gmail.com>
2013-10-03 18:13:07 -0300

    Fix interactive tour bubbles' nub.

--- static/js/misc.js
+++ static/js/misc.js
@@ -1,10 +1,10 @@
 $(window).load(function() {
-  $("#tour-steps").joyride({
-    autoStart: true,
-    template: {
-    	link: '<a href="#close" class="joyride-close-tip">×</a>',
-      button: '<a href="#" class="joyride-next-tip btn"></a>',
-      wrapper: '<div class="joyride-content-wrapper" role="dialog"><img class="joyride-kenny" src="/static/images/tooltip-action-kenny.png" alt=""></div>'
-    }
-  });
+    $("#tour-steps").joyride({
+        autoStart: true,
+        template: {
+            link: '<a href="#close" class="joyride-close-tip">×</a>',
+            button: '<a href="#" class="joyride-next-tip btn"></a>',
+            wrapper: '<div class="joyride-content-wrapper" role="dialog"><img class="joyride-kenny" src="/static/images/tooltip-action-kenny.png" alt=""></div>'
+        }
+    });
 });
